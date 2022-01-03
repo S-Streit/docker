d1d720829ec515db944d251c752594216213971d
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-01-03 12:22:12 +0100

    pathlib / up

--- hqc/start_hqc.py
+++ hqc/start_hqc.py
@@ -5,7 +5,7 @@ import os
 import argparse
 import uuid
 from datetime import datetime
-from path import Path
+from pathlib import Path
 
 def get_commit(repo_path):
     git_folder = Path(repo_path,'.git')
