28be3efd02d882fb99748245a2b6fc68301ff0cf
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-01-03 11:37:37 +0100

    default / outer command config

--- hqc/start_hqc.py
+++ hqc/start_hqc.py
@@ -5,11 +5,16 @@ import os
 import argparse
 import uuid
 
-    
-# open DEFAULT config file for "RUN-COMMAND"
-with open("usr/local/config/hqc_command_config.json") as json_file:
-    hqc_config = json.loads(json_file.read())
+outer_command_config = "usr/local/mount/config/hqc_command_config.json"
+inner_command_config = "usr/local/wrapper/default_command_config.json"
 
+# open config file for "RUN-COMMAND"
+if os.path.isfile(outer_command_config):
+    with open(outer_command_config) as json_file:
+        hqc_cmd_config = json.loads(json_file.read())
+else:
+    with open(default_command_config) as json_file:
+        hqc_cmd_config = json.loads(json_file.read())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
@@ -17,7 +22,7 @@ if __name__ == "__main__":
     #             help="one input file: example.svs",
     #             nargs="*")
 
-    parser.add_argument('-c', '--config', help="json string with config parameters: \n Defaults: {0}".format(hqc_config), default="config_light", type=str)
+    parser.add_argument('-c', '--config', help="json string with config parameters: \n Defaults: {0}".format(hqc_config), default=inner_command_config, type=str)
     parser.add_argument('-u', '--uuid', help="UUID for current algorithm run", type=str, default="")
 
     args = parser.parse_args()
@@ -26,7 +31,7 @@ if __name__ == "__main__":
         # try to read dict from json string and update default values
         try:
             config_dict = json.loads(args.config)
-            hqc_config.update(config_dict)
+            hqc_cmd_config.update(config_dict)
         except:
             print("Not a valid json config string. Using default")
     
@@ -35,21 +40,17 @@ if __name__ == "__main__":
     else:
         out_id = args.uuid
 
-    output_path = hqc_config["output_path"] + out_id # set output folder
+    output_path = hqc_cmd_config["output_path"] + out_id # set output folder
     # choose config file :
     # use config file mounted from outside OR ELSE use default file from "hqc_command_config.json"
-    outer_config_file = "usr/local/mount/config/config_adaptive.ini"
 
-    if os.path.isfile(outer_config_file):
-        config_path = outer_config_file
-    else:
-        config_path = hqc_config["config_path"]
+    config_path = hqc_cmd_config["config_path"]
 
-    base_path = "-p " + hqc_config["base_path"] if len(hqc_config["base_path"]) > 1 else "" # default in qc_pipeline: "" (empty string)
+    base_path = "-p " + hqc_cmd_config["base_path"] if len(hqc_cmd_config["base_path"]) > 1 else "" # default in qc_pipeline: "" (empty string)
     force = "-f" if hqc_config["force"] else "" # force overwrite existing output files: default in qc_pipeline: False
-    batch_size = "-b" + int(hqc_config["batch_size"]) if int(hqc_config["batch_size"]) > 0 else "" # default in config: 0 leads to default in qc_pipeline: float("inf")
-    n_threads = "-n" + int(hqc_config["n_threads"]) if int(hqc_config["n_threads"]) > 1 else "" # default in qc_pipeline: 1
-    symlink_off = "-s" if hqc_config["symlink_off"] else "" # default in qc_pipeline: True
+    batch_size = "-b" + int(hqc_cmd_config["batch_size"]) if int(hqc_cmd_config["batch_size"]) > 0 else "" # default in config: 0 leads to default in qc_pipeline: float("inf")
+    n_threads = "-n" + int(hqc_cmd_config["n_threads"]) if int(hqc_cmd_config["n_threads"]) > 1 else "" # default in qc_pipeline: 1
+    symlink_off = "-s" if hqc_cmd_config["symlink_off"] else "" # default in qc_pipeline: True
 
     # get filename from command line arguments:
 
