2f89912c6bba12e64b644ea070ab264328883a35
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-01-04 11:13:51 +0100

    add get commit func

--- hover-net/start_hovernet.py
+++ hover-net/start_hovernet.py
@@ -29,6 +29,15 @@ else:
         cmd_config = json.loads(json_file.read())
         DEFAULT_CONFIG = True
 
+
+def get_commit(repo_path):
+    git_folder = Path(repo_path,'.git')
+    head_name = Path(git_folder, 'HEAD').read_text().split('\n')[0].split(' ')[-1]
+    head_ref = Path(git_folder,head_name)
+    commit = head_ref.read_text().replace('\n','')
+
+    return commit
+
 def save_config_info(cmd_config, start_command):
 
     cfg_dict = {}
