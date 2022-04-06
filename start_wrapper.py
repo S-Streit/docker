684131c05999634dfeee8beeaf3cf860268a4636
Pacific89 <philipp.faulhammer@medunigraz.at>
2022-04-06 10:40:03 +0200

    typo / up

--- start_wrapper.py
+++ start_wrapper.py
@@ -372,7 +372,7 @@ class Wrapper():
         output_path = cmd_config["output_path"] # set output folder
         
         modelpath = cmd_config["model_path"]
-        command = "python -pp {0} -o {1} -m {2}".format(input_path, outfolder, modelpath)
+        command = "python -pp {0} -o {1} -m {2}".format(input_path, output_path, modelpath)
 
 
         start_cmd, cmd_config = self._get_simclr_command()
